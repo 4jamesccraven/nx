3ec1c6aa28df94e2078ecf1c5ffd2a7799a229f4
4jamesccraven <4jamesccraven@gmail.com>
2025-03-25 02:31:07 -0400

    first pass at shell completions

--- package.nix
+++ package.nix
@@ -9,6 +9,19 @@ rustPlatform.buildRustPackage {
 
   cargoLock.lockFile = ./Cargo.lock;
 
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
+  preFixp = ''
+    mkdir completions
+    cp $out/_nx completions
+    cp $out/nx.bash completions
+    cp $out/nx.fish completions
+
+    installShellCompletion completions/*
+  '';
+
   meta = {
     license = lib.licenses.mit;
     mainProgram = "nx";
